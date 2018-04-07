62cca35c07d6235c1009b7a6c6f4cf836aa13251
wonyong <zcx5674@gmail.com>
2018-04-07 17:32:23 +0900

    fifth problem about most length reviewerText

---
+++ src/main/java/com/kookmin/five/MostLengthReducer.java
@@ -0,0 +1,44 @@
+package com.kookmin.five;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Reducer.Context;
+
+/**
+ * 맵의 output 인자와 리듀스의 input 인자는 같아야 한다.
+ * 
+ */
+public class MostLengthReducer extends Reducer<Text, IntWritable, Text, IntWritable>{
+    private DoubleWritable result = new DoubleWritable();
+    
+    private int maxLen;
+    private String reviewerName;
+    @Override
+    protected void setup(Context context) throws IOException, InterruptedException{
+    	maxLen = context.getConfiguration().getInt("maxLen", 0);
+    	reviewerName = context.getConfiguration().get("reviewerName");
+    }
+    @Override
+    protected void reduce(Text key, Iterable<IntWritable> values,
+            Reducer<Text, IntWritable, Text, IntWritable>.Context context) throws IOException, InterruptedException {
+    	int cnt = 0;
+        for (IntWritable val : values) {
+            cnt = val.get(); 
+            if(cnt > maxLen) {
+            	maxLen = cnt;
+            	reviewerName = key.toString();
+            }
+        }
+      
+    }
+    
+    @Override
+    protected void cleanup(Context context) throws IOException, InterruptedException {
+    	 context.write(new Text(reviewerName),new IntWritable(maxLen));
+    }
+}
